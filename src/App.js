c3d6cd7f37ecfd9da809036db846268a1c20a171
Santiago <santo1996.29@gmail.com>
2020-11-03 13:52:03 -0400

    feature: container styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const FormContainer = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header title="Cotizador de Seguros" />
+    <Container>
+      <Header title="Cotizador de Seguros" />
+      <FormContainer>
+      </FormContainer>
+    </Container>
   );
 }
 
