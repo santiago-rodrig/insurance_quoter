b6d8b8edc5c484530a1710fc381ff572111bab16
Santiago <santo1996.29@gmail.com>
2020-11-03 14:08:32 -0400

    feature: styled components in Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,43 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Field = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
 
 const Form = () => {
   return (
     <form>
-      <div>
-        <label>Marca</label>
-        <select>
+      <Field>
+        <Label>Marca</Label>
+        <Select>
           <option value="">-- Seleccione --</option>
           <option value="american">Americano</option>
           <option value="european">Europeo</option>
           <option value="asian">Asiático</option>
-        </select>
-      </div>
-      <div>
-        <label>Año</label>
-        <select>
+        </Select>
+      </Field>
+      <Field>
+        <Label>Año</Label>
+        <Select>
           <option value="">-- Seleccione --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -26,13 +49,13 @@ const Form = () => {
           <option value="2014">2014</option>
           <option value="2013">2013</option>
           <option value="2012">2012</option>
-        </select>
-      </div>
-      <div>
-        <label>Plan</label>
-        <input type="radio" name="plan" value="basic" />
-        <input type="radio" name="plan" value="full" />
-      </div>
+        </Select>
+      </Field>
+      <Field>
+        <Label>Plan</Label>
+        <InputRadio type="radio" name="plan" value="basic" /> Básico
+        <InputRadio type="radio" name="plan" value="full" /> Completo
+      </Field>
       <button>Cotizar</button>
     </form>
   );
