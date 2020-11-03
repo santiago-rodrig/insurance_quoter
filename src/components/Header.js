38e0e7e9cd2fa2c279e55f4ad2ba0a910ac3b42d
Santiago <santo1996.29@gmail.com>
2020-11-03 13:49:28 -0400

    feature: styled Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const HeaderContainer = styled.header`
+  background-color: #26c6da;
+  padding: 10px;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
+const HeaderText = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+  font-family: 'Slabo 27px', serif;
+  text-align: center;
+`;
 
 const Header = ({ title }) => {
   return (
-    <h1>{title}</h1>
+    <HeaderContainer>
+      <HeaderText>
+        {title}
+      </HeaderText>
+    </HeaderContainer>
   );
 }
 
